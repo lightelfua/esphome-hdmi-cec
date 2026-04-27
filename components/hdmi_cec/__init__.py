b50261f08f87ee2f4fde86ee656b309960baa919
buga <mykola.buga@ria.com>
2026-04-27 19:06:57 +0300

    refactor

--- components/hdmi_cec/__init__.py
+++ components/hdmi_cec/__init__.py
@@ -1,76 +1,56 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import automation
-from esphome import pins
+from esphome import automation, pins
 from esphome.core import CORE
-from esphome.const import (
-    CONF_ID,
-    CONF_TRIGGER_ID,
-    CONF_DATA,
-    CONF_SOURCE,
-    CONF_ADDRESS,
-    CONF_PIN,
-    CONF_ON_MESSAGE,
-)
-
-CODEOWNERS = ["@johnboiles"]
+from esphome.const import CONF_ID, CONF_TRIGGER_ID, CONF_DATA, CONF_SOURCE, CONF_ADDRESS, CONF_PIN, CONF_ON_MESSAGE
 
 CONF_DESTINATION = "destination"
 CONF_OPCODE = "opcode"
 CONF_PHYSICAL_ADDRESS = "physical_address"
 CONF_PROMISCUOUS_MODE = "promiscuous_mode"
 
-
-def validate_raw_data(value):
-    if isinstance(value, list):
-        return cv.Schema([cv.hex_uint8_t])(value)
-    raise cv.Invalid("data must be a list of bytes")
-
-
 hdmi_cec_ns = cg.esphome_ns.namespace("hdmi_cec")
 HdmiCecComponent = hdmi_cec_ns.class_("HdmiCec", cg.Component)
 HdmiCecTrigger = hdmi_cec_ns.class_(
     "HdmiCecTrigger",
-    automation.Trigger.template(cg.std_vector.template(cg.uint8)),
+    automation.Trigger.template(cg.uint8, cg.uint8, cg.std_vector.template(cg.uint8)),
     cg.Component,
 )
 
-CONFIG_SCHEMA = cv.All(
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(HdmiCecComponent),
-            cv.Required(CONF_ADDRESS): cv.int_range(min=0, max=15),
-            cv.Optional(CONF_PHYSICAL_ADDRESS, default=0x0): cv.int_range(
-                min=0, max=0xFFFE
-            ),
-            cv.Optional(CONF_PROMISCUOUS_MODE, default=False): cv.boolean,
-            cv.Required(CONF_PIN): pins.internal_gpio_output_pin_schema,
-            cv.Optional(CONF_ON_MESSAGE): automation.validate_automation(
-                {
-                    cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(HdmiCecTrigger),
-                    cv.Optional(CONF_SOURCE): cv.int_range(min=0, max=15),
-                    cv.Optional(CONF_DESTINATION): cv.int_range(min=0, max=15),
-                    cv.Optional(CONF_OPCODE): cv.int_range(min=0, max=255),
-                    cv.Optional(CONF_DATA): cv.templatable(validate_raw_data),
-                }
-            ),
-        }
-    ).extend(cv.COMPONENT_SCHEMA),
-    cv.only_with_arduino,
-)
+def validate_raw_data(value):
+    if isinstance(value, list):
+        return cv.Schema([cv.hex_uint8_t])(value)
+    raise cv.Invalid("data must be list of bytes")
 
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(HdmiCecComponent),
+        cv.Required(CONF_ADDRESS): cv.int_range(0, 15),
+        cv.Optional(CONF_PHYSICAL_ADDRESS, default=0x0): cv.int_range(0, 0xFFFE),
+        cv.Optional(CONF_PROMISCUOUS_MODE, default=False): cv.boolean,
+        cv.Required(CONF_PIN): pins.internal_gpio_output_pin_schema,
+        cv.Optional(CONF_ON_MESSAGE): automation.validate_automation(
+            {
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(HdmiCecTrigger),
+                cv.Optional(CONF_SOURCE): cv.int_range(0, 15),
+                cv.Optional(CONF_DESTINATION): cv.int_range(0, 15),
+                cv.Optional(CONF_OPCODE): cv.int_range(0, 255),
+                cv.Optional(CONF_DATA): cv.templatable(validate_raw_data),
+            }
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 @automation.register_action(
     "hdmi_cec.send",
     hdmi_cec_ns.class_("HdmiCecSendAction", automation.Action),
-    cv.maybe_simple_value(
+    cv.Schema(
         {
             cv.GenerateID(): cv.use_id(HdmiCecComponent),
-            cv.Optional(CONF_SOURCE): cv.int_range(min=0, max=15),
-            cv.Required(CONF_DESTINATION): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_SOURCE): cv.templatable(cv.int_range(0, 15)),
+            cv.Required(CONF_DESTINATION): cv.templatable(cv.int_range(0, 15)),
             cv.Required(CONF_DATA): cv.templatable(validate_raw_data),
-        },
-        key=CONF_DATA,
+        }
     ),
 )
 async def hdmi_cec_action_to_code(config, action_id, template_arg, args):
@@ -78,67 +58,63 @@ async def hdmi_cec_action_to_code(config, action_id, template_arg, args):
     await cg.register_parented(var, config[CONF_ID])
 
     if CONF_SOURCE in config:
-        source = await cg.templatable(config[CONF_SOURCE], args, cg.uint8)
-        cg.add(var.set_source(source))
+        src = config[CONF_SOURCE]
+        if cg.is_template(src):
+            templ = await cg.templatable(src, args, cg.uint8)
+            cg.add(var.set_source_template(templ))
+        else:
+            cg.add(var.set_source(src))
 
-    destination = await cg.templatable(config[CONF_DESTINATION], args, cg.uint8)
-    cg.add(var.set_destination(destination))
+    dst = config[CONF_DESTINATION]
+    if cg.is_template(dst):
+        templ = await cg.templatable(dst, args, cg.uint8)
+        cg.add(var.set_destination_template(templ))
+    else:
+        cg.add(var.set_destination(dst))
 
     data = config[CONF_DATA]
     if isinstance(data, bytes):
-        data = [int(x) for x in data]
+        data = list(data)
+
     if cg.is_template(data):
         templ = await cg.templatable(data, args, cg.std_vector.template(cg.uint8))
         cg.add(var.set_data_template(templ))
     else:
         cg.add(var.set_data_static(data))
+
     return var
 
-
 async def to_code(config):
-    rhs = HdmiCecComponent.new()
-    var = cg.Pvariable(config[CONF_ID], rhs)
-
-    if not CORE.has_id(config[CONF_ID]):
-        var = cg.new_Pvariable(config[CONF_ID], var)
-
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
     cg.add_library(
         name="hdmi_cec",
-        repository="https://github.com//lightelfua/CEC.git",
-        version=None
+        repository="https://github.com/lightelfua/CEC.git",
     )
-    cg.add(var.set_address([config[CONF_ADDRESS]]))
-    cg.add(var.set_physical_address([config[CONF_PHYSICAL_ADDRESS]]))
-    cg.add(var.set_promiscuous_mode([config[CONF_PROMISCUOUS_MODE]]))
+
+    cg.add(var.set_address(config[CONF_ADDRESS]))
+    cg.add(var.set_physical_address(config[CONF_PHYSICAL_ADDRESS]))
+    cg.add(var.set_promiscuous_mode(config[CONF_PROMISCUOUS_MODE]))
+
     pin = await cg.gpio_pin_expression(config[CONF_PIN])
     cg.add(var.set_pin(pin))
 
     for conf in config.get(CONF_ON_MESSAGE, []):
-        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        trig = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
 
-        # Apply optional filters
-        source = conf.get(CONF_SOURCE)
-        if source is not None:
-            cg.add(trigger.set_source(source))
-        destination = conf.get(CONF_DESTINATION)
-        if destination is not None:
-            cg.add(trigger.set_destination(destination))
-        opcode = conf.get(CONF_OPCODE)
-        if opcode is not None:
-            cg.add(trigger.set_opcode(opcode))
-        data = conf.get(CONF_DATA)
-        if data is not None:
-            cg.add(trigger.set_data(data))
+        if CONF_SOURCE in conf:
+            cg.add(trig.set_source(conf[CONF_SOURCE]))
+        if CONF_DESTINATION in conf:
+            cg.add(trig.set_destination(conf[CONF_DESTINATION]))
+        if CONF_OPCODE in conf:
+            cg.add(trig.set_opcode(conf[CONF_OPCODE]))
+        if CONF_DATA in conf:
+            cg.add(trig.set_data(conf[CONF_DATA]))
 
-        await cg.register_component(trigger, conf)
+        await cg.register_component(trig, conf)
         await automation.build_automation(
-            trigger,
-            [
-                (cg.uint8, "source"),
-                (cg.uint8, "destination"),
-                (cg.std_vector.template(cg.uint8), "data"),
-            ],
+            trig,
+            [(cg.uint8, "source"), (cg.uint8, "destination"), (cg.std_vector.template(cg.uint8), "data")],
             conf,
         )
--- components/hdmi_cec/hdmi_cec.h
+++ components/hdmi_cec/hdmi_cec.h
@@ -9,7 +9,6 @@
 #include "esphome/core/log.h"
 
 #include <CEC_Device.h>
-
 #include <functional>
 
 namespace esphome {
@@ -23,28 +22,29 @@ static const uint8_t HDMI_CEC_MAX_DATA_LENGTH = 16;
 class HdmiCec : public Component, CEC_Device {
  public:
   HdmiCec(){};
-  // Component overrides
+
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::HARDWARE; }
   void loop() override;
 
-  // CEC_Device overrides
-  bool LineState() override;                                                    // NOLINT(readability-identifier-naming)
-  void SetLineState(bool state) override;                                       // NOLINT(readability-identifier-naming)
-  void OnReady(int logical_address) override;                                   // NOLINT(readability-identifier-naming)
-  void OnReceiveComplete(unsigned char *buffer, int count, bool ack) override;  // NOLINT(readability-identifier-naming)
-  void OnTransmitComplete(unsigned char *buffer, int count,
-                          bool ack) override;  // NOLINT(readability-identifier-naming)
+  bool LineState() override;
+  void SetLineState(bool state) override;
+  void OnReady(int logical_address) override;
+  void OnReceiveComplete(unsigned char *buffer, int count, bool ack) override;
+  void OnTransmitComplete(unsigned char *buffer, int count, bool ack) override;
 
   void send_data(uint8_t source, uint8_t destination, const std::vector<uint8_t> &data);
+
   void set_address(uint8_t address) { this->address_ = address; }
   void set_physical_address(uint16_t physical_address) { this->physical_address_ = physical_address; }
-  void set_promiscuous_mode(uint16_t promiscuous_mode) { this->promiscuous_mode_ = promiscuous_mode; }
+  void set_promiscuous_mode(bool promiscuous_mode) { this->promiscuous_mode_ = promiscuous_mode; }
+
   void set_pin(InternalGPIOPin *pin) {
     this->pin_ = pin;
     this->pin_->pin_mode(gpio::FLAG_INPUT);
   }
+
   void add_trigger(HdmiCecTrigger *trigger);
   static void pin_interrupt(HdmiCec *arg);
 
@@ -55,50 +55,93 @@ class HdmiCec : public Component, CEC_Device {
 
   InternalGPIOPin *pin_;
   std::vector<HdmiCecTrigger *> triggers_{};
+
   uint8_t address_;
   uint16_t physical_address_;
   bool promiscuous_mode_;
+
   HighFrequencyLoopRequester high_freq_;
-  // Used so that `pin_interrupt` doesn't fire when we're toggling the line
   volatile boolean disable_line_interrupts_ = false;
 };
 
-template<typename... Ts> class HdmiCecSendAction : public Action<Ts...>, public Parented<HdmiCec> {
+template<typename... Ts>
+class HdmiCecSendAction : public Action<Ts...>, public Parented<HdmiCec> {
  public:
-  void set_data_template(const std::function<std::vector<uint8_t>(Ts...)> func) {
+  // DATA
+  void set_data_template(const std::function<std::vector<uint8_t>(Ts...)> &func) {
     this->data_func_ = func;
-    this->static_ = false;
+    this->data_static_.reset();
   }
+
   void set_data_static(const std::vector<uint8_t> &data) {
     this->data_static_ = data;
-    this->static_ = true;
+    this->data_func_ = nullptr;
   }
-  void set_source(uint8_t source) { this->source_ = source; }
-  void set_destination(uint8_t destination) { this->destination_ = destination; }
+
+  // SOURCE
+  void set_source(uint8_t source) {
+    this->source_ = source;
+    this->source_func_ = nullptr;
+  }
+
+  void set_source_template(const std::function<uint8_t(Ts...)> &func) {
+    this->source_func_ = func;
+    this->source_.reset();
+  }
+
+  // DESTINATION
+  void set_destination(uint8_t destination) {
+    this->destination_ = destination;
+    this->destination_func_ = nullptr;
+  }
+
+  void set_destination_template(const std::function<uint8_t(Ts...)> &func) {
+    this->destination_func_ = func;
+    this->destination_.reset();
+  }
 
   void play(Ts... x) override {
-    auto source = this->source_.has_value() ? *this->source_ : this->parent_->address_;
-    if (this->static_) {
-      this->parent_->send_data(source, this->destination_, this->data_static_);
+    uint8_t source;
+    if (this->source_func_) {
+      source = this->source_func_(x...);
+    } else if (this->source_.has_value()) {
+      source = *this->source_;
     } else {
-      auto val = this->data_func_(x...);
-      this->parent_->send_data(source, this->destination_, val);
+      source = this->parent_->address_;
     }
+
+    uint8_t destination;
+    if (this->destination_func_) {
+      destination = this->destination_func_(x...);
+    } else {
+      destination = *this->destination_;
+    }
+
+    std::vector<uint8_t> data;
+    if (this->data_func_) {
+      data = this->data_func_(x...);
+    } else {
+      data = *this->data_static_;
+    }
+
+    this->parent_->send_data(source, destination, data);
   }
 
  protected:
   optional<uint8_t> source_{};
-  uint8_t destination_;
-  bool static_{false};
+  optional<uint8_t> destination_{};
+
+  std::function<uint8_t(Ts...)> source_func_{};
+  std::function<uint8_t(Ts...)> destination_func_{};
+
   std::function<std::vector<uint8_t>(Ts...)> data_func_{};
-  std::vector<uint8_t> data_static_{};
+  optional<std::vector<uint8_t>> data_static_{};
 };
 
 class HdmiCecTrigger : public Trigger<uint8_t, uint8_t, std::vector<uint8_t>>, public Component {
-  friend class HdmiCec;
-
  public:
-  explicit HdmiCecTrigger(HdmiCec *parent) : parent_(parent){};
+  explicit HdmiCecTrigger(HdmiCec *parent) : parent_(parent) {}
+
   void setup() override { this->parent_->add_trigger(this); }
 
   void set_source(uint8_t source) { this->source_ = source; }
@@ -117,4 +160,4 @@ class HdmiCecTrigger : public Trigger<uint8_t, uint8_t, std::vector<uint8_t>>, p
 }  // namespace hdmi_cec
 }  // namespace esphome
 
-#endif  // USE_ARDUINO
+#endif
